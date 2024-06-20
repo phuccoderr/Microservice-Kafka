91582af2a871449c63bf8e7ffe415d7a1b0be29f
phuccoderr <phuctapcode@gmail.com>
2024-06-20 21:55:13 +0700

    Consumer

---
+++ core/src/test/java/com/phuc/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phuc.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

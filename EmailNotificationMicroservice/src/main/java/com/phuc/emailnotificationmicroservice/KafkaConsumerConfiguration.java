5881e576cb6310210a891977d529ccd19a56745d
phuccoderr <phuctapcode@gmail.com>
2024-06-22 16:48:36 +0700

    Default Error hanlder

--- EmailNotificationMicroservice/src/main/java/com/phuc/emailnotificationmicroservice/KafkaConsumerConfiguration.java
+++ EmailNotificationMicroservice/src/main/java/com/phuc/emailnotificationmicroservice/KafkaConsumerConfiguration.java
@@ -1,6 +1,7 @@
 package com.phuc.emailnotificationmicroservice;
 
 import com.phuc.emailnotificationmicroservice.error.NotRetryableException;
+import com.phuc.emailnotificationmicroservice.error.RetryableException;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -16,6 +17,7 @@ import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -48,9 +50,10 @@ public class KafkaConsumerConfiguration {
             ConsumerFactory<String, Object> consumerFactory,
             KafkaTemplate<String, Object> kafkaTemplate) {
 
-        DefaultErrorHandler errorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate));
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate),
+                new FixedBackOff(5000,3));
         errorHandler.addNotRetryableExceptions(NotRetryableException.class);
-
+        errorHandler.addRetryableExceptions(RetryableException.class);
 
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
@@ -59,6 +62,7 @@ public class KafkaConsumerConfiguration {
         return factory;
     }
 
+    // Cau hinh de nhan loi
     @Bean
     KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
